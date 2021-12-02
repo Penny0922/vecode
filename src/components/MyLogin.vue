44ae30722901c250bde114e63cdd5f0396a75d73
Penny0922 <727449661@qq.com>
2021-12-02 11:19:37 +0800

    add vecode

--- src/components/MyLogin.vue
+++ src/components/MyLogin.vue
@@ -1,11 +1,43 @@
 <template>
-  <div class="bg">
-    <form class="testbox">
-      <h4 class="logintitle">登录界面</h4>
-      <input type="text" v-model.trim="username" placeholder="用户名" />
-      <input type="password" v-model.trim="password" placeholder="密码" />
-      <button type="button" @click="login" class="loginbtn">登录</button>
-    </form>
+  <div class="login_container">
+    <div class="login_box">
+      <h2 class="title">登录</h2>
+      <!-- 登录表单区域 -->
+      <el-form
+        ref="loginFormRef"
+        :model="loginForm"
+        :rules="loginFormRules"
+        label-width="0px"
+        class="login_form"
+      >
+        <!-- 用户名 -->
+        <el-form-item prop="username">
+          <el-input v-model="loginForm.username"></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item prop="password">
+          <el-input v-model="loginForm.password" type="password"></el-input>
+        </el-form-item>
+        <!-- 验证码 -->
+        <el-form-item prop="code" style="width: 380px">
+          <el-input
+            v-model="loginForm.code"
+            style="width: 172px; float: left"
+            maxlength="5"
+          ></el-input>
+          <el-image
+            :src="captchaImg"
+            class="captchaImg"
+            @click="getCaptcha"
+          ></el-image>
+        </el-form-item>
+        <!-- 按钮区域 -->
+        <el-form-item class="btns">
+          <el-button type="primary" @click="login">登录</el-button>
+          <el-button type="info" @click="resetLoginForm">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 
@@ -13,12 +45,19 @@
 export default {
   data() {
     return {
-      username: "毛西城",
-      password: "123456",
+      loginForm: {
+        username: "admin",
+        password: "123456",
+        code: "",
+      },
     };
   },
 
   methods: {
+    resetLoginForm() {
+      // console.log(this);
+      this.$refs.loginFormRef.resetFields();
+    },
     async login() {
       this.axios
         .post("/api/user/find", {
@@ -43,53 +82,40 @@ export default {
 </script>
 
 <style lang="less" scoped>
-.bg {
+.login_container {
   background-image: url("~@/assets/wallpaper.jpg");
   background-position: center;
   height: 100%;
   width: 100%;
   background-size: cover;
   position: fixed;
+}
 
-  .testbox {
-    position: absolute;
-    top: 50%;
-    margin-top: -200px;
-    left: 50%;
-    margin-left: -200px;
-    background-color: whitesmoke;
-    width: 400px;
-    height: 400px;
-    border-radius: 25px;
-    text-align: center;
-    padding: 5px 40px;
-    box-sizing: border-box;
-
-    input {
-      background-color: whitesmoke;
-      width: 100%;
-      height: 48px;
-      margin-bottom: 10px;
-      border: none;
-      border-bottom: 2px solid silver;
-      /* 下面的会覆盖上面的步伐 */
-      outline: none;
-      font-size: 22px;
-    }
-    .logintitle {
-      font-size: 38px;
-      font-weight: 600;
-    }
-    .loginbtn {
-      background-color: #59c2c5;
-      width: 38%;
-      height: 48px;
-      border-radius: 8px;
-      margin-top: 40px;
-      font-size: 28px;
-      font-weight: 600;
-      color: white;
-    }
+.login_box {
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 25px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+  .title {
+    padding-top: 10px;
   }
+}
+
+.login_form {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+
+.btns {
+  display: flex;
+  justify-content: flex-end;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,15 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios';
-
-
 import router from './router'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
 axios.defaults.baseURL = 'http://192.168.43.127:8888'
 // 创建 app 实例
 const app = createApp(App)
 
 app.use(VueAxios,axios)
 app.use(router)
-
+app.use(ElementPlus)
 // 挂载 app 实例
 app.mount('#app')
